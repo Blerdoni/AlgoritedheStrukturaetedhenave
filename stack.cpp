9899ce420d4cf43fc9a931861addf479af14feac
Blerdoni <blerdonpodrimqaku@icloud.com>
2024-05-29 19:16:08 +0200

    Stack

--- stack.cpp
+++ stack.cpp
@@ -14,31 +14,53 @@ void printStack(stack<int> stack){
 }
 int main(){
     //empty , size, push , pop , top 
-    stack<int>numbersStack;
+    // stack<int>numbersStack;
+    // int n;
+    // cout<<"Write the size of the Stack : ";
+    // cin>>n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int nr;
+    //     cout<<"Write a number that you want to inset into the Stack in pozition "<<i<<" : ";
+    //     cin>>nr;
+    //     numbersStack.push(nr);
+    // }
+    // cout<<endl;
+    // cout<<" { ";
+    // printStack(numbersStack);
+    // cout<<" } ";
+    // cout<<endl;
+    // cout<<"Stack size = "<<numbersStack.size()<<endl;
+    // if (numbersStack.empty())
+    // {
+    //     cout<<"Stack is empty";
+    // }
+    // else {
+    //     cout<<"Stack is not empty";
+    // }
+    stack<int>stackk;
     int n;
-    cout<<"Write the size of the Stack : ";
+    cout<<"Shenoni madhesin e stekut : ";
     cin>>n;
     for (int i = 0; i < n; i++)
     {
         int nr;
-        cout<<"Write a number that you want to inset into the Stack in pozition "<<i<<" : ";
+        cout<<"Shenoni numrin ne poziten "<<i<<" qe deshironi te shtoni ne stack : ";
         cin>>nr;
-        numbersStack.push(nr);
+        stackk.push(nr);
     }
     cout<<endl;
-    cout<<" { ";
-    printStack(numbersStack);
-    cout<<" } ";
-    cout<<endl;
-    
-    if (numbersStack.empty())
+    cout<<" {  ";
+    printStack(stackk);
+    cout<<" } "<<endl;
+    cout<<"Madhesia e stekut eshte : "<<stackk.size()<<endl;
+    if (stackk.empty())
     {
-        cout<<"Stack is empty";
+        cout<<"Steku eshte i zbrazet. ";
     }
-    else {
-        cout<<"Stack is not empty";
-    }
+    
 
+    
     
     
 
